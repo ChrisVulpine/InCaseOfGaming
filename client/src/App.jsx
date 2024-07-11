d13c794b8397caed7c0799f7458eced0e93dd0e0
Katherine-Be <katherine.bensley.93@gmail.com>
2024-07-11 17:55:51 -0400

    corrected page routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Signin from './assets/components/Signin'
+import './App.css';
+import './index.css';
+import { Outlet } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import React from 'react';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
 
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+
+  cache: new InMemoryCache(),
+});
+
+function App () {
   return (
-    <>
-    <Signin />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>InCaseOfGaming</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ApolloProvider client={client}>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    <Outlet />
+  </ApolloProvider >
   )
 }
 
-export default App
+
+
+
+export default App;
+
+
+
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Signin from './assets/components/Signin'
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//     <Signin />
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>InCaseOfGaming</h1>
+//       <div className="card">
+//         {/* <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button> */}
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,49 +1,34 @@
-import './App.css';
-import './index.css';
-import { Outlet } from 'react-router-dom';
 import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Signin from './assets/components/Signin'
+import Home from './assets/components/Home';
 
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: 'http://localhost:3000/graphql',
-});
 
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
 
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-
-  cache: new InMemoryCache(),
-});
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/login",
+        element: <Signin />
+      }
+    ]
+  }
+]);
 
 const root = createRoot(document.getElementById('root'))
 root.render(
-    
-    <ApolloProvider client={client}>
-      <App />
-      <Outlet />
-    </ApolloProvider>
-  );
+  <RouterProvider router={router} />
+);
 
 
 export default App;
