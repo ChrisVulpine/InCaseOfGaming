81c15575d86eef2aeb040c03afd97b02249e1d26
Nathan Lane <lanenathan214@gmail.com>
2024-07-11 22:34:15 -0400

    properly imported authmiddleware

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const path = require('path');
 
 
 const { ApolloServer } = require('apollo-server-express');
-const { expressMiddleware } = require('@apollo/server/express');
-const {authMiddleware} = require('./utils/auth');
-
 const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const authMiddleware = require('./utils/auth');
+// const { expressMiddleware } = require('apollo-server-express');
+// const {authMiddleware} = require('./utils/auth');
+
+
+
 //const routes = require('./routes');
 
 
