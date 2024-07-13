2ac135d2053c05cae534616f71f6481e8a3c8956
Nathan Lane <lanenathan214@gmail.com>
2024-07-12 23:57:52 -0400

    checked body header and query for token

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,9 +9,9 @@ module.exports = {
         },
     }),
     authMiddleware: function ({ req }) {
-        let token = req.body.token || req.query.token || req.headers.authorization;
+        let token = req.body?.token || req.query?.token || req.headers?.authorization;
 
-        if (req.headers.authorization) {
+        if (req.headers?.authorization) {
             token = token.split(' ').pop().trim();
         }
 
