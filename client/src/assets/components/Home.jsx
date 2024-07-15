4047278bb7443668252600f6a7a5ad5df7a9183a
Katherine-Be <katherine.bensley.93@gmail.com>
2024-07-14 21:05:37 -0400

    renamed files

--- client/src/assets/components/Home.jsx
+++ client/src/assets/components/Home.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
-import Nav from './SideBarTest'
+import Nav from './NavSideBar'
 
 import Results from './Results'
 
@@ -46,27 +46,8 @@ function Home() {
 
         {/* <div>Top Games</div>
         <div>sidebar for signedin users:wishlist/favorites</div>
-        <div>Buy Me Coffee Sidebar</div>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>InCaseOfGaming</h1>
-            <div className="card">
-                {/* <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button> 
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p> */}
+        <div>Buy Me Coffee Sidebar</div> */}
+
         </>
     )
 }
