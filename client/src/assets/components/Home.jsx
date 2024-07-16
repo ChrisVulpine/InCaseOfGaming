5887e4239999acac025905fb7c3f6d5c358104b8
Nathan Lane <lanenathan214@gmail.com>
2024-07-16 19:53:14 -0400

    moved searchbar to the homepage

--- client/src/assets/components/Home.jsx
+++ client/src/assets/components/Home.jsx
@@ -53,7 +53,22 @@ function Home() {
     return (
 
         <>
-
+        {/* moved the search bar to the homepage */}
+           <form className="mt-5 sm:flex sm:items-center" onSubmit={handleSearchSubmit}>
+                        <input 
+                        id="q" 
+                        name="q" 
+                        className="inline w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-3 leading-5 placeholder-gray-500 focus:border-indigo-500 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm" 
+                        placeholder="Keyword" 
+                        type="search" 
+                        autoFocus="" 
+                        value={searchQuery} 
+                        onChange={(e)=> setSearchQuery(e.target.value)} 
+                        />
+                        <button type="submit" className="mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 md:font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                            Search
+                        </button>
+                    </form>
             {/* <Header class="z-20"/> */}
             <Nav style={navIndex}/>
 
