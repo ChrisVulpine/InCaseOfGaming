030fb6d0dc4471060d9f38e5e7409f3438d4f231
Nathan Lane <lanenathan214@gmail.com>
2024-07-16 23:40:15 -0400

    changed api that we're using to gamedetails, changed the limit of 10

--- client/src/utils/gameDataFetch.js
+++ client/src/utils/gameDataFetch.js
@@ -8,18 +8,15 @@ async function fetchGameDetails(searchValue) {
     }
     
     const options = {
-        method: 'GET',
-        // change URL to new api
-        url: 'https://steam2.p.rapidapi.com/search/Counter/page/1',
-            params: {
-            query: searchValue,
-            limit: '2'
-        },
-        headers: {
-          'x-rapidapi-key': '023fef78bdmsh20edf95ffe57816p1d555ajsn66f4aae9c3ee',
-          'x-rapidapi-host': 'steam2.p.rapidapi.com'
-        }
-      };
+      url: `https://games-details.p.rapidapi.com/search/${encodeURIComponent(searchValue)}`,
+      params: {
+        limit: '10'
+      },
+      headers: {
+        'x-rapidapi-key': '023fef78bdmsh20edf95ffe57816p1d555ajsn66f4aae9c3ee',
+        'x-rapidapi-host': 'games-details.p.rapidapi.com'
+      }
+    };
 
       try {
     
