305e0aefb9fbb237bd336a6d916b9adae00936e4
Nathan Lane <lanenathan214@gmail.com>
2024-07-18 12:48:45 -0400

    deleted console log

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,7 +10,6 @@ module.exports = {
     }),
     authMiddleware: function ({ req }) {
         let token = req.body?.token || req.query?.token || req.headers?.authorization;
-        console.log(token);
 
 
         if (req.headers?.authorization) {
